955ee4cf3e04e09ae9c88aac165cbd1bde25b83a
rolandcucicea <rolandcucicea@protonmail.com>
2023-04-06 00:18:31 +0100

    added functionality to the "Delete" button using event delegation

--- script.js
+++ script.js
@@ -1,41 +1,44 @@
 const submitButton = document.getElementById("submit");
 
 function childButtons() {
-    const edit = document.createElement("button");
-    const del = document.createElement("button");
-    edit.textContent = "Edit";
-    edit.id = "edit-btn"
-    del.textContent = "Delete";
-    del.id = "del-btn";
-    return {
-        editButton: edit,
-        deleteButton: del
-    };
+  const edit = document.createElement("button");
+  const del = document.createElement("button");
+  edit.textContent = "Edit";
+  edit.id = "edit-btn";
+  del.textContent = "Delete";
+  del.id = "del-btn";
+  return {
+    editButton: edit,
+    deleteButton: del,
+  };
 }
 
 function addTasks(task) {
-    const listElement = document.createElement("li");
-    listElement.textContent = task;
+  const listElement = document.createElement("li");
+  listElement.textContent = task;
 
-    const buttons = childButtons();
-    listElement.appendChild(buttons.editButton);
-    listElement.appendChild(buttons.deleteButton);
+  const buttons = childButtons();
+  listElement.appendChild(buttons.editButton);
+  listElement.appendChild(buttons.deleteButton);
 
-    const taskList = document.getElementById('task-list');
-    taskList.appendChild(listElement);
-    return taskList;
+  const taskList = document.getElementById("task-list");
+  taskList.appendChild(listElement);
+  return taskList;
 }
 
-function editTask() {
-    
+function editTask() {}
+
+function deleteTask(e) {
+  if (e.target.matches("#del-btn")) {
+    const task = e.target.closest("li");
+    task.remove();
+  }
 }
 
-function deleteTask() {
-
-}
-
-submitButton.addEventListener('click', () => {
-    const task = document.getElementById('task').value;
-    addTasks(task);
+submitButton.addEventListener("click", () => {
+  const task = document.getElementById("task").value;
+  addTasks(task);
 });
 
+const taskList = document.querySelector("#task-list");
+taskList.addEventListener("click", (e) => deleteTask(e));
