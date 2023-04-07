0e8857f3d3c67a6013b6ce3f9748c347d169fe87
rolandcucicea <rolandcucicea@protonmail.com>
2023-04-07 01:32:35 +0100

    more styling, improved functionality

--- script.js
+++ script.js
@@ -15,21 +15,38 @@ function childButtons() {
 
 function addTasks(task) {
   const listElement = document.createElement("li");
-  const spanContainer = document.createElement('span');
+  const spanContainer = document.createElement("span");
   spanContainer.textContent = task;
   listElement.appendChild(spanContainer);
 
-  const buttonContainer = document.createElement('span');
+  const buttonContainer = document.createElement("span");
   const buttons = childButtons();
   buttonContainer.appendChild(buttons.editButton);
   buttonContainer.appendChild(buttons.deleteButton);
 
   listElement.appendChild(buttonContainer);
-  
 
   const taskList = document.getElementById("task-list");
   taskList.appendChild(listElement);
   return taskList;
+}
+function editTask(task) {
+  const originalFormat = task.cloneNode(true);
+  const inputField = document.createElement("input");
+  inputField.value = task.firstChild.textContent;
+  task.replaceChild(inputField, task.firstChild);
+  inputField.focus();
+  inputField.select();
+
+  const editButton = task.querySelector("span").firstChild;
+  const doneButton = editButton.cloneNode(true);
+  doneButton.id = "done-btn";
+  doneButton.textContent = "Done";
+  task.querySelector("span").replaceChild(doneButton, editButton);
+  doneButton.addEventListener("click", (e) => {
+    originalFormat.firstChild.textContent = inputField.value;
+    task.parentNode.replaceChild(originalFormat, task);
+  });
 }
 
 function buttonFunctionalities(e) {
@@ -37,18 +54,8 @@ function buttonFunctionalities(e) {
     const task = e.target.closest("li");
     task.remove();
   } else if (e.target.matches("#edit-btn")) {
-    const inputTask = document.createElement("input");
-    const listElement = e.target.closest("li");
-    inputTask.value = listElement.firstChild.textContent;
-    listElement.replaceWith(inputTask);
-    inputTask.addEventListener("keydown", (e) => {
-      if (e.key === "Enter") {
-        const newText = document.createTextNode(inputTask.value);
-        inputTask.replaceWith(listElement);
-        const oldText = listElement.firstChild;
-        listElement.replaceChild(newText, oldText);
-      }
-    });
+    const spanListElement = e.target.parentNode.parentNode;
+    editTask(spanListElement);
   }
 }
 
